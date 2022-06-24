bafe9c77b5a7d9987bae0a3b2aa90a870a77d593
AnnaTutaj <anna.tutaj@outlook.com>
2022-06-24 22:33:17 +0200

    Allow custom className in HeaderText component

--- src/common/components/HeaderText/HeaderText.tsx
+++ src/common/components/HeaderText/HeaderText.tsx
@@ -3,10 +3,13 @@ import cn from 'classnames';
 export interface IHeaderTextProps {
   text: string;
   size?: 'small';
+  className?: string;
 }
 
-const HeaderText: React.FC<IHeaderTextProps> = ({ text, size }) => {
-  return <span className={cn(styles.HeaderText, { [styles.HeaderTextSmall]: size === 'small' })}>{text}</span>;
+const HeaderText: React.FC<IHeaderTextProps> = ({ text, size, className }) => {
+  return (
+    <span className={cn(styles.HeaderText, { [styles.HeaderTextSmall]: size === 'small' }, className)}>{text}</span>
+  );
 };
 
 export default HeaderText;
