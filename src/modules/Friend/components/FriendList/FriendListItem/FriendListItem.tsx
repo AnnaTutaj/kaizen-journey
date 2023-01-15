e467aa764d288c25b11acbca9d3c855f1f899784
AnnaTutaj <anna.tutaj@outlook.com>
2023-01-15 20:25:08 +0100

    Fix styles after upgrade to antd v5

--- src/modules/Friend/components/FriendList/FriendListItem/FriendListItem.tsx
+++ src/modules/Friend/components/FriendList/FriendListItem/FriendListItem.tsx
@@ -91,12 +91,12 @@ const FriendListItem: React.FC<IProps> = ({ friend, mode, removeFriendFollowing,
         </Col>
         <Col>
           {mode === 'following' && removeFriendFollowing ? (
-            <Button type="ghost" onClick={() => confirmDeleteFollowing()} size="middle">
+            <Button type="primary" ghost onClick={() => confirmDeleteFollowing()} size="middle">
               {intl.formatMessage({ id: 'friend.following.form.unfollow' })}
             </Button>
           ) : null}
           {mode === 'follower' && removeFriendFollower ? (
-            <Button type="ghost" onClick={() => confirmDeleteFollower()} size="middle">
+            <Button type="primary" ghost onClick={() => confirmDeleteFollower()} size="middle">
               {intl.formatMessage({ id: 'friend.follower.list.delete' })}
             </Button>
           ) : null}
