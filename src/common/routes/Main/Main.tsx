7a5a71367c0e05abe9c964855697673daee19f48
AnnaTutaj <anna.tutaj@outlook.com>
2023-01-16 21:30:11 +0100

    Route-based code splitting
    Link: https://reactjs.org/docs/code-splitting.html

--- src/common/routes/Main/Main.tsx
+++ src/common/routes/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Layout } from 'antd';
 import { generatePath, Navigate, Route, Routes, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -6,27 +6,28 @@ import { ILayoutOwnState } from '@common/redux/modules/Layout/LayoutInterface';
 import Header from '@common/containers/Header';
 import Footer from '@common/containers/Footer';
 import { Paths } from '@common/constants/Paths';
-import PageUnderConstruction from '@common/components/PageUnderConstruction';
 import styles from './Main.module.less';
 import { useAuth } from '@common/contexts/AuthContext';
 import PageLoading from '@common/components/PageLoading';
-import Home from '@modules/Home';
-import Gratitude from '@modules/Gratitude';
 import PrivateRoute from '@common/containers/PrivateRoute';
-import Habit from '@modules/Habit';
 import HabitTracker from '@modules/Habit/routes/HabitTracker';
 import HabitArchive from '@modules/Habit/routes/HabitArchive';
-import Friend from '@modules/Friend';
 import FriendFollowing from '@modules/Friend/routes/FriendFollowing';
 import FriendFollower from '@modules/Friend/routes/FriendFollower';
 import HabitView from '@modules/Habit/routes/HabitView';
 import cn from 'classnames';
 import { useIntl } from 'react-intl';
-import User from '@modules/User';
 import UserHabit from '@modules/User/routes/UserHabit';
 import UserGratitude from '@modules/User/routes/UserGratitude';
 import UserFriendFollowing from '@modules/User/routes/UserFriendFollowing';
 import UserFriendFollower from '@modules/User/routes/UserFriendFollower';
+
+const PageUnderConstruction = lazy(() => import('@common/components/PageUnderConstruction'));
+const Home = lazy(() => import('@modules/Home'));
+const Gratitude = lazy(() => import('@modules/Gratitude'));
+const Habit = lazy(() => import('@modules/Habit'));
+const Friend = lazy(() => import('@modules/Friend'));
+const User = lazy(() => import('@modules/User'));
 
 const { Content } = Layout;
 //todo add route to PageNotFound, AccessDenied
@@ -49,69 +50,71 @@ const Main: React.FC = () => {
     <Layout>
       <Header />
       <Content className={cn(styles.Content, { [styles.ContentNoPadding]: hideContentPadding })}>
-        <Routes>
-          <Route path={Paths.Home} element={<Home />} />
-          <Route
-            path={Paths.Gratitude}
-            element={
-              <PrivateRoute>
-                <Gratitude />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path={Paths.Habit}
-            element={
-              <PrivateRoute>
-                <Habit />
-              </PrivateRoute>
-            }
-          >
-            <Route path={Paths.HabitTracker} element={<HabitTracker />} />
-            <Route path={Paths.HabitArchive} element={<HabitArchive />} />
-            <Route path={Paths.Habit} element={<Navigate replace to={Paths.HabitTracker} />} />
-          </Route>
-          <Route
-            path={Paths.HabitView}
-            element={
-              <PrivateRoute>
-                <HabitView />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path={Paths.Friend}
-            element={
-              <PrivateRoute>
-                <Friend />
-              </PrivateRoute>
-            }
-          >
-            <Route path={Paths.FriendFollowing} element={<FriendFollowing />} />
-            <Route path={Paths.FriendFollowers} element={<FriendFollower />} />
+        <Suspense fallback={<PageLoading />}>
+          <Routes>
+            <Route path={Paths.Home} element={<Home />} />
             <Route
-              path={Paths.FriendFollowers}
-              element={<PageUnderConstruction title={intl.formatMessage({ id: 'friend.menu.followers' })} />}
+              path={Paths.Gratitude}
+              element={
+                <PrivateRoute>
+                  <Gratitude />
+                </PrivateRoute>
+              }
             />
-            <Route path={Paths.Friend} element={<Navigate replace to={Paths.FriendFollowing} />} />
-          </Route>
-          <Route
-            path={Paths.UserView}
-            element={
-              <PrivateRoute>
-                <User />
-              </PrivateRoute>
-            }
-          >
-            <Route path={Paths.UserViewHabit} element={<UserHabit />} />
-            <Route path={Paths.UserViewGratitude} element={<UserGratitude />} />
-            <Route path={Paths.UserViewFollowing} element={<UserFriendFollowing />} />
-            <Route path={Paths.UserViewFollowers} element={<UserFriendFollower />} />
-            <Route path={Paths.UserView} element={<RedirectToUser />} />
-          </Route>
-          {/* todo: Create Page Not Found View */}
-          <Route path="*" element={<PageUnderConstruction title="Page Not Found" />} />
-        </Routes>
+            <Route
+              path={Paths.Habit}
+              element={
+                <PrivateRoute>
+                  <Habit />
+                </PrivateRoute>
+              }
+            >
+              <Route path={Paths.HabitTracker} element={<HabitTracker />} />
+              <Route path={Paths.HabitArchive} element={<HabitArchive />} />
+              <Route path={Paths.Habit} element={<Navigate replace to={Paths.HabitTracker} />} />
+            </Route>
+            <Route
+              path={Paths.HabitView}
+              element={
+                <PrivateRoute>
+                  <HabitView />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path={Paths.Friend}
+              element={
+                <PrivateRoute>
+                  <Friend />
+                </PrivateRoute>
+              }
+            >
+              <Route path={Paths.FriendFollowing} element={<FriendFollowing />} />
+              <Route path={Paths.FriendFollowers} element={<FriendFollower />} />
+              <Route
+                path={Paths.FriendFollowers}
+                element={<PageUnderConstruction title={intl.formatMessage({ id: 'friend.menu.followers' })} />}
+              />
+              <Route path={Paths.Friend} element={<Navigate replace to={Paths.FriendFollowing} />} />
+            </Route>
+            <Route
+              path={Paths.UserView}
+              element={
+                <PrivateRoute>
+                  <User />
+                </PrivateRoute>
+              }
+            >
+              <Route path={Paths.UserViewHabit} element={<UserHabit />} />
+              <Route path={Paths.UserViewGratitude} element={<UserGratitude />} />
+              <Route path={Paths.UserViewFollowing} element={<UserFriendFollowing />} />
+              <Route path={Paths.UserViewFollowers} element={<UserFriendFollower />} />
+              <Route path={Paths.UserView} element={<RedirectToUser />} />
+            </Route>
+            {/* todo: Create Page Not Found View */}
+            <Route path="*" element={<PageUnderConstruction title="Page Not Found" />} />
+          </Routes>
+        </Suspense>
       </Content>
       <Footer />
     </Layout>
