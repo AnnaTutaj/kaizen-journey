2d4293217ae4517aa9ac3dbc848748b2b88e21fc
AnnaTutaj <anna.tutaj@outlook.com>
2023-02-02 22:20:23 +0100

    Add various sizes to spinner

--- src/common/components/Spinner/Spinner.tsx
+++ src/common/components/Spinner/Spinner.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import styles from './Spinner.module.less';
+import cn from 'classnames';
 
-const Spinner: React.FC = () => {
+export interface ISpinnerProps {
+  size?: 'small' | 'middle' | 'large';
+}
+
+const Spinner: React.FC<ISpinnerProps> = ({ size = 'large' }) => {
   const count = 4;
 
   return (
-    <div className={styles.Spinner}>
+    <div
+      className={cn(styles.Spinner, {
+        [styles.SpinnerSmall]: size === 'small',
+        [styles.SpinnerMiddle]: size === 'middle',
+        [styles.SpinnerLarge]: size === 'large'
+      })}
+    >
       {[...Array(count)].map((value, index) => (
         <span className={styles[`Dot_${index + 1}`]} key={index} />
       ))}
