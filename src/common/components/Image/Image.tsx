a8c8e146591ee443ec0bb5e958d2505c47953f84
AnnaTutaj <anna.tutaj@outlook.com>
2023-02-05 12:36:48 +0100

    Style image: - add spinner when image is loading - set min-width to display preview icon (on broken image)

--- src/common/components/Image/Image.tsx
+++ src/common/components/Image/Image.tsx
@@ -1,7 +1,8 @@
 import { Image as AntdImage, ImageProps } from 'antd';
+import Spinner from '../Spinner';
 
 const Image: React.FC<ImageProps> = ({ src, ...props }) => {
-  return <AntdImage {...props} src={src} />;
+  return <AntdImage {...props} src={src} placeholder={<Spinner size="middle" />} />;
 };
 
 export default Image;
