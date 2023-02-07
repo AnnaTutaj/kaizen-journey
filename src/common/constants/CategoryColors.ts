78ef7afba7373a972ce7135e34e1f8e3f51866d4
AnnaTutaj <anna.tutaj@outlook.com>
2023-02-07 21:19:32 +0100

    Add more colors to color picker: - refactor styles with less list functions (each)
    Links:
    https://lesscss.org/functions/#functions-overview
    https://lesscss.org/functions/#list-functions-each

--- src/common/constants/CategoryColors.ts
+++ src/common/constants/CategoryColors.ts
@@ -1,17 +1,46 @@
 export enum CategoryColors {
-  default = '#73B8AD',
-  red = '#BF626D',
-  green = '#B2CF76',
-  purple = '#9D95CF',
-  brown = '#C4A270'
+  default = '#25b78b',
+  red = '#b94242',
+  fuchsia = '#b94290',
+  pink = '#cf86b6',
+  purple = '#8e86c2',
+  blue = '#2ea8f5',
+  navy = '#2929b9',
+  green = '#2a931f',
+  olive = '#93a738',
+  yellow = '#cda70e',
+  orange = '#d87732',
+  brown = '#bb9153',
+  gray = '#8a8a8a'
 }
 
 export enum CategoryColorsLighten {
-  default = '#95CFC5',
-  red = '#D6838D',
-  green = '#CDE69A',
-  purple = '#C2BCE6',
-  brown = '#DBBE93'
+  default = '#3ed7a9',
+  red = '#c86666',
+  fuchsia = '#c866a6',
+  pink = '#d89ec4',
+  purple = '#a49ece',
+  blue = '#57b9f7',
+  navy = '#4545d5',
+  green = '#39c92a',
+  olive = '#b0c553',
+  yellow = '#f0c725',
+  orange = '#df925b',
+  brown = '#c8a775',
+  gray = '#a1a1a1'
 }
 
-export type CategoryColorsDTO = 'default' | 'red' | 'green' | 'purple' | 'brown';
+export type CategoryColorsDTO =
+  | 'default'
+  | 'red'
+  | 'fuchsia'
+  | 'pink'
+  | 'purple'
+  | 'blue'
+  | 'navy'
+  | 'green'
+  | 'olive'
+  | 'yellow'
+  | 'orange'
+  | 'brown'
+  | 'gray';
