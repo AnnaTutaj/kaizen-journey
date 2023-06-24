66e1ce45683e525aa5055d2219dcedd5dec80f9c
AnnaTutaj <anna.tutaj@outlook.com>
2023-06-24 16:30:11 +0200

    Migrate to styled-components: Spinner

--- src/common/components/Spinner/Spinner.tsx
+++ src/common/components/Spinner/Spinner.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import styles from './Spinner.module.less';
-import cn from 'classnames';
+import { StyledDot, StyledSpinner } from './styled';
 
+export type SpinnerSize = 'small' | 'middle' | 'large';
 export interface ISpinnerProps {
-  size?: 'small' | 'middle' | 'large';
+  size?: SpinnerSize;
 }
 
 const Spinner: React.FC<ISpinnerProps> = ({ size = 'large' }) => {
   const count = 4;
 
   return (
-    <div
-      className={cn(styles.Spinner, {
-        [styles.SpinnerSmall]: size === 'small',
-        [styles.SpinnerMiddle]: size === 'middle',
-        [styles.SpinnerLarge]: size === 'large'
-      })}
-    >
+    <StyledSpinner $size={size}>
       {[...Array(count)].map((value, index) => (
-        <span className={styles[`Dot_${index + 1}`]} key={index} />
+        <StyledDot key={index} $index={index +1} />
       ))}
-    </div>
+    </StyledSpinner>
   );
 };
 
