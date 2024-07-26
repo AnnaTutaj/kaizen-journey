c2066e5c351821a252e85cadff6bac57352d7e8f
AnnaTutaj <anna.tutaj@outlook.com>
2024-07-26 21:00:51 +0200

    Fix building project after migrate to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
-import browserslistToEsbuild from 'browserslist-to-esbuild'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
+import mkcert from 'vite-plugin-mkcert';
+import svgr from 'vite-plugin-svgr';
+import path from "path";
 
 export default defineConfig({
   base: '/',
@@ -10,9 +12,16 @@ export default defineConfig({
     open: true,
     port: 3000
   },
-  plugins: [react(), viteTsconfigPaths(), mkcert()],
+  plugins: [react(), viteTsconfigPaths(), mkcert(), svgr()],
   build: {
     outDir: 'build',
     target: browserslistToEsbuild()
+  },
+  resolve: {
+    alias: [
+      { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+      { find: '@common', replacement: path.resolve(__dirname, './src/common') },
+      { find: '@modules', replacement: path.resolve(__dirname, './src/modules') }
+    ]
   }
 });
